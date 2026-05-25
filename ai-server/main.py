1e0467701e7ba608564d5bd95b88765ff34c9835
dean <wi04707@gmail.com>
2026-05-25 21:24:59 +0900

    feat: connect fastapi llm response with spring, db, and frontend

--- ai-server/main.py
+++ ai-server/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+#from langchain_openai import ChatOpenAI
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
 
 app = FastAPI()
+
+#llm = ChatOpenAI(model="gpt-4o-mini")
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7
+)
 
 class AskRequest(BaseModel):
     question: str
@@ -14,6 +27,23 @@ def hello():
     return {"message": "hello fastapi"}
 
 #request = AskRequest(question="안녕?")
-@app.post("/ai/ask")
+@app.post("/ai/ask", response_model=AskResponse)
 def ask(request: AskRequest):
-    return AskResponse(answer=f"Python AI 서버가 받은 질문:{request.question}")
+    try:
+        prompt = f"""
+        너는 친절한 AI 도우미다.
+        답변은 한국어로 간결하고 이해하기 쉽게 작성해라.
+
+        사용자 질문:
+        {request.question}
+        """
+         
+        #response = llm.invoke(request.question)
+        response = llm.invoke(prompt)
+        return AskResponse(answer=response.content)
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
